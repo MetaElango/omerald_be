3811372391c01c137674810341379b1ae5cdc010
Elangodevguy <elangodevguy@gmail.com>
2022-04-25 11:05:29 +0530

    added starter file

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const uploadRouter = require("./router");
+
+const app = express();
+
+app.get("/", (_, res) => {
+  console.log(__dirname);
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(uploadRouter);
+
+app.listen(8080, () => {
+  console.log("Form running on port 8080");
+});
